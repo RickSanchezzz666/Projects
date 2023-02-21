07f01bb8fc0ad3c895f3633c9fd0a8ae2a073ed2
RickSanchezzz666 <lapiyktube@gmail.com>
2023-02-21 15:29:06 +0200

    commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require('dotenv').config()
+
+
+console.log(`MONGO_DB_URI:${process.env.MONGO_DB_URI}`)
+console.log(`PORT:${process.env.PORT}`)
+
+const Mongo = require('./setup/mongoose')
 
 const app = express();
 app.use(bodyParser);
 
-app.listen(8080, () => {
-    console.log("Server was started on 8080 port.")
-});
+const setup = async () => {
+    await Mongo.setupDb(process.env.MONGO_DB_URI);
+    app.listen(process.env.PORT, () => {
+        console.log("Server was started on 8080 port.")
+    });
+}
+
+setup();
