91292ba28e949635a5e2da038960899240ad2795
RickSanchezzz666 <lapiyktube@gmail.com>
2023-02-21 19:59:56 +0200

    commit

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ console.log(`PORT:${process.env.PORT}`)
 const Mongo = require('./setup/mongoose')
 
 const app = express();
-app.use(bodyParser);
+app.use(bodyParser.json());
 
 const setup = async () => {
     await Mongo.setupDb(process.env.MONGO_DB_URI);
 
+    app.post("/comments", async (req, res) => {
+        const { name, email, text } = req.body;
+
+        const doc = new Comments({
+            name, email, text, date: new Date()
+        });
+
+        const elem = await doc.save()
+
+        return res.status(200).send(elem);
+    });
+
     app.get("/comments", async (req, res) => {
-        const docs = await Comments.find({}).limit(20).exec();
+        const docs = await Comments.find().limit(20);
         return res.status(200).send(docs);
-    })
+    });
 
     app.listen(process.env.PORT, () => {
         console.log("Server was started on 8080 port.")
