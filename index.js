01378020f6789ca7d34f4ad9eac7339801bbc54c
RickSanchezzz666 <lapiyktube@gmail.com>
2023-02-21 15:40:31 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require('dotenv').config()
+const { Comments } = require('./models/comments')
 
 
 console.log(`MONGO_DB_URI:${process.env.MONGO_DB_URI}`)
@@ -13,6 +14,12 @@ app.use(bodyParser);
 
 const setup = async () => {
     await Mongo.setupDb(process.env.MONGO_DB_URI);
+
+    app.get("/comments", async (req, res) => {
+        const docs = await Comments.find({}).limit(20).exec();
+        return res.status(200).send(docs);
+    })
+
     app.listen(process.env.PORT, () => {
         console.log("Server was started on 8080 port.")
     });
