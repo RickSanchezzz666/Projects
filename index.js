353cf327803b6702a6df76b88fb7d8f815e2c72d
RickSanchezzz666 <lapiyktube@gmail.com>
2023-03-03 09:37:55 +0200

    s

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require('dotenv').config()
-const sessions = require('./api/sessions.api')
+
+
+require("./models/links")
+require("./models/users")
 
 
 console.log(`MONGO_DB_URI:${process.env.MONGO_DB_URI}`)
@@ -14,21 +17,6 @@ app.use(bodyParser.json());
 
 const setup = async () => {
     await Mongo.setupDb(process.env.MONGO_DB_URI);
-
-    app.use(sessions.router);
-
-
-    /*app.post("/sessions", async (req, res) => {
-        const { name, email, text } = req.body;
-
-        const doc = new Sessions({
-            name, email, text, date: new Date()
-        });
-
-        const elem = await doc.save()
-
-        return res.status(200).send(elem);
-    });*/
 
     app.listen(process.env.PORT, () => {
         console.log("Server was started on 8080 port.")
