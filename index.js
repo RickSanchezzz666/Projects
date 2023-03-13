f679eb6be0b44a37be0ed0512737e543c2bc7f28
RickSanchezzz666 <lapiyktube@gmail.com>
2023-03-13 09:49:33 +0200

    yoyoyoyoyo betch

--- index.js
+++ index.js
@@ -7,21 +7,40 @@ const scenario = fs.readFileSync('./scenario.txt', (err, data) => {
     }
     console.log(data);
 })
-const resultio = scenario.toString();
-const results = resultio.match(/^[a-z]+:/gmi);
+const resultNames = scenario.toString();
+const results_1 = resultNames.match(/^[a-z]+:/gmi);
+
+const resultText = scenario.toString();
+const results_2 = resultText.match(/[a-z]+.+[?,;.:!]/gmi);
 
 const charachters = [];
-results.forEach(characterName => {
+const charachtersSpeeches = [];
+results_1.forEach(characterName => {
  const name = characterName.slice(0, -1);
- // [].find(e => ...) => null || elem
- // [].includes(<value>) => boolean
- if (!charachters.includes(name)) {
-  charachters.push(name);
- }
-});
+    if (!charachters.includes(name)) {
+      charachters.push(name);
+    }
+})
+
+results_2.forEach(texts => {
+  const text = texts.slice(-5, 0);
+  
+  if(text.match(charachters[0])) {
+    charachtersSpeeches[0].push(texts)
+  }
+  if(text.match(charachters[1])) {
+    charachtersSpeeches[1].push(texts)
+  }
+  if(text.match(charachters[2])) {
+    charachtersSpeeches[2].push(texts)
+  }
+  if(text.match(charachters[3])) {
+    charachtersSpeeches[3].push(texts)
+  }
+})
 
 console.log(charachters);
 
 for(let num = 0; num < charachters.length; num += 1) {
-    fs.writeFileSync(`${charachters[num]}.txt`, 'blablabla')
+      fs.writeFileSync(`${charachters[num]}.txt`, `${charachtersSpeeches[num]}`)
 }
