a03aceb90fc50d086f51ce5a9858a5d6d3b65a2b
RickSanchezzz666 <lapiyktube@gmail.com>
2023-03-13 15:14:13 +0200

    FINALLY SUCCESS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { text } = require('body-parser');
 const fs = require('fs')
 
 const scenario = fs.readFileSync('./scenario.txt', (err, data) => {
@@ -16,31 +17,58 @@ const results_2 = resultText.match(/[a-z]+.+[?,;.:!]/gmi);
 const charachters = [];
 const charachtersSpeeches = [];
 results_1.forEach(characterName => {
- const name = characterName.slice(0, -1);
+ const name = characterName.slice(0, -1)
     if (!charachters.includes(name)) {
       charachters.push(name);
     }
 })
 
+let charachtersSpeeches0 = '';
+let charachtersSpeeches1 = '';
+let charachtersSpeeches2 = '';
+let charachtersSpeeches3 = '';
+
 results_2.forEach(texts => {
-  const text = texts.slice(-5, 0);
-  
-  if(text.match(charachters[0])) {
-    charachtersSpeeches[0].push(texts)
+  let result = texts.match(/^[a-z]+:/gmi);
+  let notArray = result[0]
+  let resSlice = notArray.slice(0, -1);
+
+  if(resSlice === `Max`) {
+    charachtersSpeeches0 += texts;
+    charachtersSpeeches0 += ' \n';
   }
-  if(text.match(charachters[1])) {
-    charachtersSpeeches[1].push(texts)
+  if(resSlice === 'Geralt') {
+    charachtersSpeeches1 += texts;
+    charachtersSpeeches1 += ' \n';
   }
-  if(text.match(charachters[2])) {
-    charachtersSpeeches[2].push(texts)
+  if(resSlice === 'Yennefer') {
+    charachtersSpeeches2 += texts;
+    charachtersSpeeches2 += ' \n';
   }
-  if(text.match(charachters[3])) {
-    charachtersSpeeches[3].push(texts)
+  if(resSlice === 'Triss') {
+    charachtersSpeeches3 += texts;
+    charachtersSpeeches3 += ' \n';
   }
 })
+
+charachtersSpeeches.push(charachtersSpeeches0);
+charachtersSpeeches.push(charachtersSpeeches1);
+charachtersSpeeches.push(charachtersSpeeches2);
+charachtersSpeeches.push(charachtersSpeeches3);
 
 console.log(charachters);
 
 for(let num = 0; num < charachters.length; num += 1) {
-      fs.writeFileSync(`${charachters[num]}.txt`, `${charachtersSpeeches[num]}`)
+  if(num === 0) {
+    fs.writeFileSync(`${charachters[num]}.txt`, charachtersSpeeches[num])
+  }
+  if(num === 1) {
+    fs.writeFileSync(`${charachters[num]}.txt`, charachtersSpeeches[num])
+  }
+  if(num === 2) {
+    fs.writeFileSync(`${charachters[num]}.txt`, charachtersSpeeches[num])
+  }
+  if(num === 3) {
+    fs.writeFileSync(`${charachters[num]}.txt`, charachtersSpeeches[num])
+  }
 }
